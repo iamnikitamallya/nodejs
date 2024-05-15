e47c4ad930d407c2104b6c145716312b18285c9e
iamnikitamallya <mallyanikita165@gmail.com>
2024-05-16 00:11:20 +0100

    adding commonjs example file

---
+++ commonjsmodule.js
@@ -0,0 +1,8 @@
+function sum() {
+    const a = "This is";
+  const b = "Nikita"
+  const result = a + " " + b;
+  return result;
+}
+// console.log(sum());
+module.exports = sum;
--- fsmodules.js
+++ fsmodules.js
@@ -1,5 +1,18 @@
-const fs = require("node:fs");
+const fs = require("fs");
+const sum = require('./commonjsmodule')
 
-fs.readFile('file.txt', 'utf8', (err, data) => {
-    console.log(err, data)
+// fs.readFile('file.txt', 'utf8', (err, data) => {
+//     // console.log(err, data)
+// })
+
+// const a = fs.readFileSync('file.txt');
+// console.log(a.toString());
+
+// console.log(sum())
+
+const b = fs.writeFile('file2.text', sum(), () => {
+    console.log(sum())
 })
+
+
+// console.log("This is a nodejs beginner project")
